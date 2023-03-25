c52e728afca0585db56fd3abd91dcfb47f48be94
Hansen Makangiras <hansenmakangiras@gmail.com>
2023-03-25 20:30:23 +0800

    [ADD] bump

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import fs from "fs";
+import {defineConfig} from "vite";
+import laravel from "laravel-vite-plugin";
+// import { viteStaticCopy } from "vite-plugin-static-copy";
+import {homedir} from "os";
+import {resolve} from "path";
 
+let host = "laravel10.local";
 export default defineConfig({
     plugins: [
+        // viteStaticCopy({
+        //     targets: [
+        //         {
+        //             src: "resources/images",
+        //             dest: "assets",
+        //         },
+        //         {
+        //             src: "resources/json",
+        //             dest: "assets",
+        //         },
+        //     ],
+        // }),
         laravel({
             input: [
                 'resources/css/app.css',
@@ -11,4 +28,33 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: detectServerConfig(host),
 });
+
+function detectServerConfig(host) {
+    let keyPath = resolve(
+        homedir(),
+        "D:/Development/laragon/etc/ssl/laragon.key"
+    );
+    let certificatePath = resolve(
+        homedir(),
+        "D:/Development/laragon/etc/ssl/laragon.crt"
+    );
+
+    if (!fs.existsSync(keyPath)) {
+        return {};
+    }
+
+    if (!fs.existsSync(certificatePath)) {
+        return {};
+    }
+
+    return {
+        hmr: {host},
+        host,
+        https: {
+            key: fs.readFileSync(keyPath),
+            cert: fs.readFileSync(certificatePath),
+        },
+    };
+}
